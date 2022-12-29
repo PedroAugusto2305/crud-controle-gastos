fffc0afd1379c28f1b9514553ce519d677cd740e
Pedro Augusto <p.augusto_23@outlook.com>
2022-12-29 18:07:20 -0300

    :sparkles: feat: Mensagem de erro no formulário

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-function validateFields() {
-  const emailValid = isEmailValid();
-  document.getElementById('recoverPassword').disabled = !emailValid;
-
-  const passwordValid = isPasswordValid();
-  document.getElementById('loginBtn').disabled = !emailValid || !passwordValid;
+function onChangeEmail() {
+  toggleButtonDisable()
+  toggleEmailErrors()
 }
-
+function onChangePassword() {
+  toggleButtonDisable()
+  togglePasswordErrors()
+}
 function isEmailValid() {
   const email = document.getElementById('email').value;
   if (!email) {
@@ -13,6 +13,39 @@ function isEmailValid() {
   }
   return validateEmail(email);
 }
+
+function toggleEmailErrors() {
+  const email = document.getElementById('email').value;
+  if (!email) {
+    document.getElementById('emailRequiredError').style.display = 'block';
+  } else {
+    document.getElementById('emailRequiredError').style.display = 'none';
+  }
+
+  if (validateEmail(email)) {
+    document.getElementById('emailInvalidError').style.display = 'none';
+  } else {
+    document.getElementById('emailInvalidError').style.display = 'block';
+  }
+}
+
+function togglePasswordErrors() {
+  const password = document.getElementById('password').value;
+  if (!password) {
+    document.getElementById('passwordRequiredError').style.display = 'block';
+  } else {
+    document.getElementById('passwordRequiredError').style.display = 'none';
+  }
+}
+
+function toggleButtonDisable() {
+  const emailValid = isEmailValid();
+  document.getElementById('recoverPassword').disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  document.getElementById('loginBtn').disabled = !emailValid || !passwordValid;
+}
+
 
 function isPasswordValid() {
   const password = document.getElementById('password').value;
