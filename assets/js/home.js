6ad5193c6cefd351fc5ebc0711ba2e6f7c728874
Pedro Augusto <p.augusto_23@outlook.com>
2023-01-07 19:53:25 -0300

    :construction: Dashboard em construção

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,3 +5,15 @@ function logout() {
     alert('Erro ao fazer logout!')
   })
 }
+
+// menu toggle
+function closeMenu() {
+  const toggle = document.querySelector('.hamburger');
+  const sideMenu = document.querySelector('.side-menu');
+  const main = document.querySelector('#home');
+
+  toggle.onclick = () => {
+    sideMenu.classList.toggle('active')
+    main.classList.toggle('active')
+  }
+}
